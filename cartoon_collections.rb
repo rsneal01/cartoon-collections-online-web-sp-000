673f6346be3794be0c5f86ce30cab39fdf4388a1
Bobby Neal <rsneal01@gmail.com>
2020-05-04 17:19:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,6 +20,7 @@ def long_planeteer_calls(long_calls)
 end
 
 def find_the_cheese(cheese_array)
-  cheese_types.find? do |string|
+  cheese_types.find do |string|
+end
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
